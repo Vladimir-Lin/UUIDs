226c70d1d1f6df5df2767b7f0ab9ae797506c81e
foxman <lin.vladimir@gmail.com>
2019-03-05 16:43:35 +0800

    Move Private Convoy into Windows Directory

---
+++ SRCs/Windows/PrivateUUIDs.hpp
@@ -0,0 +1,16 @@
+/****************************************************************************\
+ *
+ *                         SUID and TUID private functions
+ *
+\****************************************************************************/
+
+#ifndef PRIVATEUUIDS_HPP
+#define PRIVATEUUIDS_HPP
+
+#include "UUIDs.hpp"
+
+#include <list>
+#include <map>
+
+
+#endif
